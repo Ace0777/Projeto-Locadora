8d2240705a5b65903de683f509fcbf2f2e96667f
Ace0777 <luishhasantos@gmail.com>
2023-05-21 22:45:05 -0300

    atualizando classes do packege locadora

--- Locadora/src/main/java/br/com/locadora/model/locadora/Locadora.java
+++ Locadora/src/main/java/br/com/locadora/model/locadora/Locadora.java
@@ -8,13 +8,42 @@ import java.util.ArrayList;
 
 public class Locadora {
 
-    private ArrayList<Funcionario> funcionarios;
+   private int id;
+
+   private String nome;
+
+   private String endereco;
 
     public Locadora(){
 
     }
-    public Locadora(ArrayList<Locacao> locacaos, ArrayList<Funcionario> funcionarios, ArrayList<Cliente> clientes) {
-        this.locacaos = locacaos;
-        this.funcionarios = funcionarios;
+    public Locadora(int id, String nome, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
 }
